4aa3d48269cf51e48e26a2d168c86ea662a1bac1
Isidro Butaslac <butaslaciii@gmail.com>
2023-11-14 19:48:19 +0800

    take a lot of samples then shutdown

--- save_results.sh
+++ save_results.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+# hide imshow
 for p in {0..3}; do
     for b in {0..3}; do
         for t in 1 2 4 8 16; do
 
-        python3.11 benchmark_cpu.py "-p $p" "-b $b" "-t $t" -mc 300 -f
+        python3.11 benchmark_cpu.py "-p $p" "-b $b" "-t $t" -mc 1000 -f
         pid=$!
         wait $pid
         sleep 3
@@ -12,3 +13,19 @@ for p in {0..3}; do
         done
     done
 done
+
+# show imshow
+for p in {0..3}; do
+    for b in {0..3}; do
+        for t in 1 2 4 8 16; do
+
+        python3.11 benchmark_cpu.py "-p $p" "-b $b" "-t $t" -mc 1000
+        pid=$!
+        wait $pid
+        sleep 3
+
+        done
+    done
+done
+
+sudo shutdown -h now
